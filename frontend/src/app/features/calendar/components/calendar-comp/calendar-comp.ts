72ee2fbe41fa58f4d4e3be5a30f77848ffeab60e
Phillip Wood <phillipwood2023@outlook.com>
2026-05-02 15:23:45 -0400

    Displaying days on calendar. However, small bug with date's not matching days of the week on some months.

--- frontend/src/app/features/calendar/components/calendar-comp/calendar-comp.ts
+++ frontend/src/app/features/calendar/components/calendar-comp/calendar-comp.ts
@@ -11,14 +11,15 @@ export class CalendarComp {
   protected currentDate = new Date();
   protected months = new Map<number, string>([[0, 'January'], [1, 'February'], [2, 'March'], [3, 'April'], [4, 'May'], [5, 'June'], [6, 'July'], [7, 'August'], [8, 'September'], [9, 'October'], [10, 'November'], [11, 'December']]);
   protected years = [this.currentDate.getFullYear(), this.currentDate.getFullYear() + 1];
+  protected daysOfMonth: number[] = new Array<number>();
 
   selectedMonth = signal(this.currentDate.getMonth());
   selectedYear = signal(this.currentDate.getFullYear());
 
-  getNumDays(month: number, year: number): number {
+  getNumDays(month: number, year: number): number[] {
     const firstDay = new Date(year, month, 1);
     const lastDay = new Date(year, month + 1, 0);
-    return lastDay.getDate();
+    return Array.from({length: lastDay.getDate()}, (_, i) => i + 1);
   }
 
   getFirstDayOffset(month: number, year: number): number {
@@ -45,6 +46,7 @@ export class CalendarComp {
 
   selectMonth(month: number) {
     this.selectedMonth.set(month);
+    this.daysOfMonth = this.getNumDays(month, this.selectedYear());
     this.toggleMonths();
   }
 
