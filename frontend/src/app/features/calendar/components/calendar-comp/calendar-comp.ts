0799c340fc72b0736456ff64b4117083412bc2ec
Phillip Wood <phillipwood2023@outlook.com>
2026-05-02 13:23:49 -0400

    Toggling years and months lists for calender and updating selected year and month.

--- frontend/src/app/features/calendar/components/calendar-comp/calendar-comp.ts
+++ frontend/src/app/features/calendar/components/calendar-comp/calendar-comp.ts
@@ -10,7 +10,7 @@ export class CalendarComp {
 
   protected currentDate = new Date();
   protected months = new Map<number, string>([[0, 'January'], [1, 'February'], [2, 'March'], [3, 'April'], [4, 'May'], [5, 'June'], [6, 'July'], [7, 'August'], [8, 'September'], [9, 'October'], [10, 'November'], [11, 'December']]);
-  protected years = [2026, 2027];
+  protected years = [this.currentDate.getFullYear(), this.currentDate.getFullYear() + 1];
 
   selectedMonth = signal(this.currentDate.getMonth());
   selectedYear = signal(this.currentDate.getFullYear());
@@ -28,22 +28,29 @@ export class CalendarComp {
   toggleMonths() {
     const div = document.getElementById('month-list-container');
     if (div) {
-      div.style.display = div.style.display === 'block' ? 'none' : 'block';
+      div.style.visibility = div.style.visibility === 'hidden' ? 'visible' : 'hidden';
     }else {
       console.log('div is null');
     }
   }
 
   toggleYears() {
-    document.getElementById('month-list-container ')?.setAttribute('display', 'block');
+    const div = document.getElementById('year-list-container');
+    if (div) {
+      div.style.visibility = div.style.visibility === 'hidden' ? 'visible' : 'hidden';
+    }else {
+      console.log('div is null');
+    }
   }
 
   selectMonth(month: number) {
     this.selectedMonth.set(month);
+    this.toggleMonths();
   }
 
   selectYear(year: number) {
     this.selectedYear.set(year);
+    this.toggleYears();
   }
 
 }
